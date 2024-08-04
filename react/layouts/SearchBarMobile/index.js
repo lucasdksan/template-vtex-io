0f5f3c928be70ac17ec840084cefb1a8af19a460
Lucas <lucas.silva@agencian1.com.br>
2024-08-04 14:00:06 -0300

    feat: header do template

--- react/layouts/SearchBarMobile/index.js
+++ react/layouts/SearchBarMobile/index.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 import { classGenerator } from "../../utils/classGenerator";
 
+import styles from "./styles.module.css";
+
 const SearchBarMobile = ({ SearchBar, SearchBarIcon, SearchBarContainer }) => {
     const [show, setShow] = useState(false);
     const classButton = classGenerator("search-bar-mobile", "button");
 
     const handleToggle = () => setShow(prev => !prev);
 
-    return show
-        ? (
-            <SearchBarContainer>
-                <SearchBar />
-                <button className={classButton} onClick={handleToggle}>Fechar</button>
-            </SearchBarContainer>
-        )
-        : <button className={classButton} onClick={handleToggle}><SearchBarIcon /></button>;
+    return(
+        <>  
+            { show && (
+                <SearchBarContainer>
+                    <SearchBar />
+                    <button className={`${classButton} ${styles.btn}`} onClick={handleToggle}>Fechar</button>
+                </SearchBarContainer>
+            ) }
+            <button className={`${classButton} ${styles.btn}`} onClick={handleToggle}><SearchBarIcon /></button>
+        </>
+    );
 }
 
 export default SearchBarMobile;
